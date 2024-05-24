5412ab5f12823f8183134c06aee6204363802ef6
Jesus Soto <soleje2862004@gmail.com>
2024-05-24 09:19:41 -0600

    Termino del formulario Datos Generales y creacion de checklist

--- server/controllers/programasController.js
+++ server/controllers/programasController.js
@@ -3,6 +3,7 @@ const Programas = require('../models/programasSchema');
 const obtenerProgramas = async (req, res) => {
   try {
     const programas = await Programas.find();
+    console.log(programas); // Verificar que se obtienen los datos correctos
     res.status(200).json(programas);
   } catch (error) {
     res.status(500).json({ error: 'Error interno del servidor' });
